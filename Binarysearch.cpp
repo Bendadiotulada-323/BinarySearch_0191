bd1ca8bc4a50afb213bb9054ff1b4a8a39d7f9ad
Bendadiotulada-323 <bendadiotulada@gmail.com>
2026-04-17 14:47:21 +0700

    Else tidak boleh lebih dari 10. Eror or try again

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -15,4 +15,10 @@ void input()
         if (npanjang <= 10)
         {
             break;
-        
+        }
+        else
+        {
+            cout << "\n[!] jumlah elemen tidak boleh lebih dari 10. Silahkan coba lagi.\n";
+        } 
+    }
+}
